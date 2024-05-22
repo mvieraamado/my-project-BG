afa0082cd184da0b339e550402004cacf1220d09
mviera <mviera@baufest.com>
2024-05-22 15:13:25 -0300

    add service

---
+++ src/services/service.js
@@ -0,0 +1,23 @@
+import axios from axios;
+
+const API_URL = 'http://127.0.0.1:8000/api/product';
+
+export const getProducts = ()=> {
+    return axios.get(API_URL);
+}
+
+export const getProduct = ()=> {
+    return axios.get(`${API_URL}${id}/`);
+}
+
+export const createProduct = (product)=> {
+    return axios.post(API_URL, product);
+}
+
+export const updateProduct = (id, product)=> {
+    return axios.put(`${API_URL}${id}/`, product);
+}
+
+export const deleteProduct = (id)=> {
+    return axios.delete(`${API_URL}${id}/`);
+}
